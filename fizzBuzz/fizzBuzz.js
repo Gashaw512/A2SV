1a86c7a359d97fb5a140b7296352fb5a8ce53673
Gashaw Kidanu <kidanugashaw@gmail.com>
2023-01-11 19:28:21 +0300

    organized file and solution with js also added

---
+++ fizzBuzz/fizzBuzz.js
@@ -0,0 +1,18 @@
+var fizzBuzz = function(n) {
+    let ans=[]
+    for (let i=1; i<=n; i++){
+        let divby3=(i%3==0)
+        let divby5=(i%5==0)
+        if(divby3 && divby5)
+        ans.push("FizzBuzz")
+        else if(divby3)
+        ans.push("Fizz")
+        else if(divby5)
+        ans.push("Buzz")
+        else
+        ans.push(""+i)
+    }
+    return ans
+};
+
+console.log( fizzBuzz(6))
