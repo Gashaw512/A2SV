4bb2d61d63d5634cca70f3390b7a4040a48c9cd2
Gashaw Kidanu <kidanugashaw@gmail.com>
2023-01-12 14:56:34 +0300

    refactor and some errors corrected

--- leetcode/sorting/mergeInterval.js
+++ leetcode/sorting/mergeInterval.js
@@ -3,34 +3,25 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
-    // for one coordinate no overlap
-    if(intervals.length<2)
-    return intervals
+         // for one coordinate no overlap
+  if (intervals.length < 2) return intervals;
 
   let notOverlaps = [];
-//   it sorts the interval based on the initial point
-  intervals.sort((a,b)=>a[0]-b[0])
-// previos is array with size of 2 and it initialy hold the first coordinate previos=[x,y]
-  let previous=intervals[0]
-  
-  for(let i=0; i<intervals.length; i++){
-     // previos=[x,y], previous[1]=y. then check if 
-     console.log("...........")
-     console.log(previous[1])
-     console.log(intervals[i][0])
-     console.log("..........")
-    if(previous[1]>=intervals[i][0]){
-        previous = [previous[0], Math.max(previous[1], intervals[i][1])];
-    }
-    else{
-       notOverlaps.push(previous);
-        previous = intervals[i];
+         //   it sorts the interval based on the initial point
+  intervals.sort((a, b) => a[0] - b[0]);
+         // previos is array with size of 2 and it initialy hold the first coordinate previos=[x,y]
+  let previous = intervals[0];
+  for (let i = 1; i < intervals.length; i++) {
+        // previos=[x,y], previous[1]=y. then check if y is greater than the x of the second coordinate
+    if (previous[1] >= intervals[i][0]) {
+      previous = [previous[0], Math.max(previous[1], intervals[i][1])];
+    } else {
+      notOverlaps.push(previous);
+      previous = intervals[i];
     }
   }
 
-notOverlaps.push(previous)
+  notOverlaps.push(previous);
 
-  return notOverlaps
+  return notOverlaps;
 };
-// let a=[[1,3],[2,6],[8,10],[15,18]]
-// console.log(merge(a))
