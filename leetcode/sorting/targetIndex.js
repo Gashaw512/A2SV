13ccd82fea295941c4086b0498fbc85473b65779
Gashaw Kidanu <kidanugashaw@gmail.com>
2023-01-12 11:24:48 +0300

    technical error resolved

--- leetcode/sorting/targetIndex.js
+++ leetcode/sorting/targetIndex.js
@@ -4,10 +4,13 @@
  * @return {number[]}
  */
 var targetIndices = function(nums, target) {
-  nums.sort()
+    // technical error resolved
+    // Sorting an array in numerical order
+ let sorted=nums.sort((a, b) => a - b)
   let indices=[];
-  for(let i=0; i<nums.length; i++){
-    if(nums[i]===target){
+  for(let i=0; i<sorted.length; i++){
+    console.log(nums[i])
+    if(sorted[i]===target){
         indices.push(i)
     }
   }
@@ -15,5 +18,5 @@ return indices;
 };
 // test code
 
-// let a=[1,2,5,2,3]
-// targetIndices(a,5)
+// let a=[48,90,9,21,31,35,19,69,29,52,100,54,21,86,6,45,42,5,62,77,15,38]
+// console.log(targetIndices(a,6))
